f2b401289eb69d53d15653c0de260e7b36821e21
Aleksandr K <kuchumof@gmail.com>
2023-03-06 14:52:47 +0500

    add var c

--- src/java/Test1.java
+++ src/java/Test1.java
@@ -3,6 +3,9 @@ public class Test1 {
         int[] num = new int[]{1, 2, 3, 4, 5};
         for (int i = 0; i < num.length; i++) {
             System.out.println(num[i]);
+
         }
+        int c = 8;
+        System.out.println(c);
     }
 }
