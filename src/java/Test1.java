8bde3cbaff545d56b31104c1d09798cbb7a41b4a
Aleksandr K <kuchumof@gmail.com>
2023-03-06 14:37:40 +0500

    start new project

---
+++ src/java/Test1.java
@@ -0,0 +1,8 @@
+public class Test1 {
+    public static void main(String[] args) {
+        int[] num = new int[]{1, 2, 3, 4, 5};
+        for (int i = 0; i < num.length; i++) {
+            System.out.println(num[i]);
+        }
+    }
+}
